307536de988cd26071b628d7638f86e8890e77aa
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-10-21 20:38:36 +0300

    Working on hw-5

---
+++ src/components/Definitions.jsx
@@ -0,0 +1,13 @@
+<dl>
+    <dt>one</dt>
+    <dd>two</dd>
+    <dt>another term</dt>
+    <dd>another description</dd>
+</dl>
+
+<dl>
+    <dt>Coffee</dt>
+    <dd>Black hot drink</dd>
+    <dt>Milk</dt>
+    <dd>White cold drink</dd>
+</dl>
