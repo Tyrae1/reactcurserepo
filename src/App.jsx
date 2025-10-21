d314459277735594960638f36b9dc203a199c316
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-10-21 19:57:28 +0300

    Finishing hw-4

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Card from './components/Card';
+
+function App() {
+
+  return (
+      <>
+          <Card title="Hello all!"/>
+          <Card text="lorem ipsum dolor"/>
+          <Card title="Hello all!" text="lorem ipsum dolor"/>
+      </>
+  );
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+class Card extends Component {
+    render() {
+        const {title, text} = this.props;
+        return (
+            <div className="card">
+                <div className="card-body">
+                    {title && <h4 className="card-title">{title}</h4>}
+                    {text && <p className="card-text">{text}</p>}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Card;
