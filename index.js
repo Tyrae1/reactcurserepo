8db1c967938e3d7dce1352e68384055e6e6a2275
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-10-25 09:58:33 +0300

    Finishing hw-2

--- index.js
+++ index.js
@@ -4,36 +4,42 @@ class ContextMaze {
    constructor(name) {
        this.name = name;
    }
-   log(){
-       console.log(this.name);
+   static createChained(){
+       const a = new ContextMaze("A");
+       const b = new ContextMaze("B");
+       a.log();
+       b.log();
+       a.nested(b.log);
+       b.nested(a.delayLog);
+       const lost = a.log;
+       lost.call(b);
    }
-
-   delaylog() {
-       setTimeout(function (){
-           console.log('delaylog anonimous:', this && this.name);
-       }, 1000);
-       setTimeout(()=>{
-           console.log('delaylog arrow:', this.name);
-       }, 1000);
-       setTimeout(function (){
-           console.log('delaylog bind:', this.name);
-       }.bind(this), 1000);
+   get log(){
+       const owner = this;
+       function fn(){console.log(this.name);}
+       fn.owner = owner;
+       return fn;
+   }
+   get delayLog() {
+       const owner = this;
+       function fn(){
+           const self = this;
+           setTimeout(function (){
+               console.log('delayLog anonimous:', self.name);
+           }, 1000);
+           setTimeout(()=>{
+               console.log('delayLog arrow:', this.name);
+           }, 1000);
+           setTimeout(function (){
+               console.log('delayLog bind:', this.name);
+           }.bind(this), 1000);
+       }
+       fn.owner = owner;
+       return fn;
    }
    nested(fn){
-      const boundFn = fn.bind(this);
-      return boundFn();
-   }
-   createChained(){
-
+       const ctx = fn.owner || this;
+       return fn.call(ctx);
    }
 }
-
-const a = new ContextMaze("A");
-const b = new ContextMaze("B");
-
-a.log();
-b.log();
-a.nested(b.log);
-b.nested(a.delaylog);
-const lost = a.log;
-lost.call(b);
+ContextMaze.createChained();
