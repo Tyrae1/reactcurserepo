b0719aca271adb48a4fc9633fe6f5a09fea9b421
Kostiantin Gordiienko <bulletproofua94@gmail.com>
2025-11-18 21:00:35 +0200

    Creating hw-11

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-new main
+Необхідно написати React APP Todo list
+
+Бібліотеки котрі треба використовувати
+
+- https://react-bootstrap.netlify.app/
+- https://formik.org/
+- https://www.npmjs.com/package/prop-types
+- https://www.npmjs.com/package/prop-types
+
+Працювати має аналогічно цьому можна без збереження стану після перезавантаження сторінки.
+
+https://vladimirshaitan.github.io/TodoListForStudents/
+
+Використовувати Formik обов'язково 
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { Container, Row, Col, Card, Form, Button, ButtonGroup, ListGroup, Badge } from 'react-bootstrap';
+
+
+function App() {
+
+  return (
+    <div className="py-4 bg-light min-vh-100">
+    <Container>
+        <Row className="justify-content-center mb-4">
+            <Col xs={12} md={8} lg={6}>
+                <h1 className="text-center mb-3">Todo List (HW11)</h1>
+            </Col>
+        </Row>
+    </Container>
+    </div>
+  )
+}
+
+export default App
